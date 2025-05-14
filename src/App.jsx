f4d9a7f4c7bf6e657fd0b5735d8afa5c0eb9dac6
Samama Zaid <s.zaid43111@gmail.com>
2025-05-14 17:46:06 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import { use, useState } from "react";
+import styles from "./App.module.css";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
+import { Assistant } from "./components/assistants/googleai";
+// import { Assistant } from "./components/assistants/openai";
+// import { Assistant } from "./components/assistants/deepseekai";
+import { Loader } from "./components/Loader/Loader";
+
+
+function App() {
+  const assistant = new Assistant();
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isStreaming, setStreaming] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message])
+  }
+  function updateLastMessageContent(content) {
+    setMessages((prevMessages) => prevMessages.map((message, index) => index === prevMessages.length - 1 ? { ...message, content: `${message.content}${content}` } : message));
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: 'user' });
+    setLoading(true);
+    try {
+      const result = await assistant.chatStream(content, messages);
+      let isFirstChunk = false;
+      for await (const chunk of result) {
+        if (!isFirstChunk) {
+          isFirstChunk = true;
+          setLoading(false);
+          setStreaming(true);
+          addMessage({ content: "", role: "assistant" });
+        }
+        updateLastMessageContent(chunk);
+      }
+      setStreaming(false);
+      // const result = await assistant.chat(content, messages);
+      // addMessage({ content: result, role: "assistant" })
+    } catch (error) {
+      addMessage({ content: "Sorry, I can't process your query right now!Try Again later", role: "system" })
+    }
+    setLoading(false);
+    setStreaming(false);
+  }
+  const toggleTheme = () => {
+    setLoading(true); // Start loading
+
+    setTimeout(() => {
+      setIsDarkMode(!isDarkMode);
+
+      if (!isDarkMode) {
+
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+        document.body.style.setProperty("color-scheme", "dark");
+      } else {
+
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+        document.body.style.setProperty("color-scheme", "light");
+      }
+
+      setLoading(false);
+    }, 300);
+  };
+
+  return (
+    <div className={styles.App}>
+      {isLoading && <Loader />
+      }
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="robot.png" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+        <button onClick={toggleTheme} className={styles.ThemeButton}>
+          {isDarkMode ? '🌞' : '🌙'}
+        </button>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls isDisabled={isLoading || isStreaming} onSend={handleContentSend} />
+    </div>
+  );
+}
+// const MESSAGES = [
+//   {
+//     role: "user",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "assistant",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "user",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "assistant",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "user",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "assistant",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "user",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+//   {
+//     role: "assistant",
+//     content:
+//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?",
+//   },
+// ];
+
+
+export default App;
---
+++ src/components/assistants/openai.js
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPEN_AI_API_KEY,
+    dangerouslyAllowBrowser: true
+});
+
+export class Assistant {
+    #model
+    #client
+    constructor(model = "gpt-3.5-turbo", client = openai) {
+        this.#model = model;
+        this.#client = client;
+    }
+
+    async chat(content, history) {
+        try {
+            const result = await this.#client.chat.completions.create({
+                model: this.#model,
+                messages: [...history, { content, role: 'user' }]
+            })
+            return result.choices[0].message.content;
+        } catch (error) {
+            throw (error);
+        }
+    }
+    async *chatStream(content, history) {
+        try {
+            const result = await this.#client.chat.completions.create({
+                model: this.#model,
+                messages: [...history, { content, role: 'user' }],
+                stream: true
+            })
+            for await (const chunk of result) {
+                yield chunk.choices[0]?.delta?.content || ""
+            }
+        } catch (error) {
+            throw (error);
+        }
+    }
+}
